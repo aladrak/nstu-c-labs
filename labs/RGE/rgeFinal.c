f3b3b29c2155181625273ca2efd3b9cb1f97959e
aladrak <sashakhong@gmail.com>
2025-01-05 23:01:32 +0700

    finally

--- labs/RGE/rgeFinal.c
+++ labs/RGE/rgeFinal.c
@@ -1,5 +1,6 @@
 #include <stdio.h> // gcc rgeFinal.c -o rgeFinal.exe
 
+#define DOUBLE_LEN 64
 #define MANTISSA_LEN 52
 #define EXP_LEN 11
 
@@ -7,35 +8,50 @@
 #define BITS_52 0x000FFFFFFFFFFFFF
 #define BITS_11 0x7FF
 
-double degree(double n, double s) {
-    double res = 1;
-    for (int i = 0; i < (s < 0 ? -s : s); i++) res *= n;
-    return (s < 0 ? 1 / res : res);
-}
+void printArr(unsigned *a, int len) { for (int i = len - 1; i >= 0; i--) printf("%u", a[i]); }
 
-double corrector(double n) { 
-    if (n < 0) return n - 1;
-    if (n > 0) return n + 1;
-}
-void printArr(unsigned *a, int len) {
-    for (int i = len - 1; i >= 0; i--) printf("%u", a[i]);
-}
-void printArrRight(unsigned *a, int len) {
-    for (int i = 0; i < len; i++) printf("%u", a[i]);
-}
-void printBinDouble(unsigned long long l) {
+void printArrRight(unsigned *a, int len) { for (int i = 0; i < len; i++) printf("%u", a[i]); }
+void printBin(unsigned long long l, int low, int high) {
     int counter = 0;
-    for (int i = BYTE * BYTE - 1; i >= 0; i--) {
-        printf("%u%c", (l >> i) & 1, (counter == 0 || counter == 11) * ' '); counter++;
+    for (int i = high - 1; i >= low; i--) {
+        printf("%u", (l >> i) & 1); counter++;
+    }
+}
+void transDecToBin(int n) {
+    short a[32] = {0};
+    if (n == 0) { 
+        printf("0");
+        return;
+    }
+    int i = 0;
+    while (n > 0) {
+        a[i++] = (n % 2);
+        n /= 2;
+    }
+    while(i >= 0) {
+        printf("%u", a[i--]);
     }
 }
 
-// int sign(unsigned long long l) { return (l >> (MANTISSA_LEN + EXP_LEN)) & 1; }
+int _sign(unsigned long long l) { return (l >> (MANTISSA_LEN + EXP_LEN)) & 1; } 
 
-// int _exp(unsigned long long l) { return (l >> MANTISSA_LEN) & BITS_11; }
+int _exp(unsigned long long l) { return ((l >> MANTISSA_LEN) & BITS_11) - 1023; }
 
-// unsigned long long _mantissa(unsigned long long l) { return l & BITS_52; }
+unsigned long long _mantissa(unsigned long long l) { return l & BITS_52; }
 
+void fracBin(double n, unsigned arr[], unsigned floatPart[]) {
+    int i = MANTISSA_LEN - 1; 
+    double fracPart = n - (int)n;
+    do {
+        fracPart *= 2;
+        int r = (int)fracPart;
+        fracPart -= r;
+        arr[i--] = r;
+    } while (fracPart > 0 && i >= 0);
+    for (int i = 0; i < MANTISSA_LEN; i++) {
+        floatPart[i] = arr[i];
+    }
+}
 void multiplyBinary(unsigned *m, int len_m, unsigned *n, int len_n, unsigned *overflow) {
     int carry = 0, temp_result[MANTISSA_LEN * 2] = {0};
 
@@ -68,16 +84,15 @@ unsigned binaryToDecimal(unsigned *a, int len) {
     return dec;
 }
 
-// double bytesToDouble(unsigned long long l) {
-//     double val = 1.;
-//     if (_exp(l) != 0) {
-//         printf("(1 + %llu / 2^52)", _mantissa(l));
-//         val += _mantissa(l) / degree(2, MANTISSA_LEN);
-//         printf(" * 2^%d\n", (int)_exp(l) - 1023);
-//         val *= degree(2, (int)_exp(l) - 1023);
-//     } else return 0; 
-//     return (degree(-1, sign(l)) * val);
-// }
+unsigned long long extractIntegerPart(unsigned long long mantissa, int exponent) {
+    mantissa |= 0x0010000000000000;
+    if (exponent >= 0) {
+        mantissa <<= exponent;
+    } else {
+        mantissa >>= -exponent;
+    }
+    return mantissa >> 52;
+}
 
 void main () {
     union 
@@ -86,40 +101,40 @@ void main () {
         unsigned long long int ll; // 8 byte
         double d; // 8 byte
     } uni;
-    printf("Enter number: "); scanf("%lf", &uni.d);
-    uni.d  = 0.1; uni.d = ((int)uni.d == 0 ? corrector(uni.d) : uni.d);
-    unsigned m[MANTISSA_LEN], n[4] = {0, 1, 0, 1}, overflow[MANTISSA_LEN * 2] = {0};
-    for (int i = MANTISSA_LEN - 1; i >= 0; i--) {
-        m[i] = (uni.ll >> i) & 1;
+    printf("1. "); scanf("%lf", &uni.d);
+    
+    int exps = _exp(uni.ll); unsigned long long mant = _mantissa(uni.ll);
+    unsigned m[MANTISSA_LEN], n[4] = {0, 1, 0, 1}, overflow[MANTISSA_LEN * 2] = {0}, floatPart[MANTISSA_LEN];
+    fracBin(uni.d, m, floatPart);
+    if ((int)uni.d != 0) {
+        for (int i = MANTISSA_LEN - 1; i >= 0; i--) {
+            floatPart[i] = m[i] = ((uni.ll << exps) >> i) & 1;
+        }
     }
-    printf("\nOriginal m[]: "); printArr(m, MANTISSA_LEN);
 
     unsigned ofBit[100];
     int counter = 0;
     while (!isArrayZero(m, MANTISSA_LEN)) {
         multiplyBinary(m, MANTISSA_LEN, n, 4, overflow);
 
-        printf("\nResult m[]: ");
-        printArr(m, MANTISSA_LEN);
+        // printf("\nResult m[]: ");
+        // printArr(m, MANTISSA_LEN);
 
         ofBit[counter++] = binaryToDecimal(overflow, 6);
-        printf("\nOverflow (extra bits): %u ", binaryToDecimal(overflow, 6));
-        printArr(overflow, 4);
+        // printf("\nOverflow (extra bits): %u ", binaryToDecimal(overflow, 6));
+        // printArr(overflow, 4);
     }
 
-    printf("\n\norigin num: %.52lf", uni.d - 1);
-    printf("\noverflowed num: 0."); printArrRight(ofBit, counter);
+    printf("2. binary view: "); printBin(uni.ll, 0, DOUBLE_LEN);
+    printf("\n3.1 sign: %d", _sign(uni.ll));
+    printf("\n3.2 exp: %d ", _exp(uni.ll)); printBin(uni.ll, DOUBLE_LEN - EXP_LEN, DOUBLE_LEN - 1);
+    printf("\n3.3 mantissa: "); printBin(uni.ll, 0, DOUBLE_LEN - EXP_LEN - 1);
+    printf("\n4.1 "); transDecToBin(extractIntegerPart(mant, exps));
+    printf("\n4.2 "); printArr(floatPart, MANTISSA_LEN);
+    printf("\n5. %llu.", extractIntegerPart(mant, exps)); printArrRight(ofBit, counter);
 
-    double res = 0.;
-    for (int i = 0; i < counter; i++) res += degree(10, -i-1) * ofBit[i];
-
-    printf("\nresult: %.52lf\n%.52lf\n", res, res - 0.1);
-
-    printf("binary view: "); printBinDouble(uni.ll); printf("\n");
-    // looks pretty fine
-    for (int i = 0; i < sizeof("binary view: ") - 1; i++) printf(" ");
-    for (int i = sizeof(double) - 1; i >= 0; i--) printf("^___%u%c__^", i, (i == 7 || i == 6) * '_');
-
-    // printf("decimal view-1: %.25lf\n", *(double*)uni.bytes);
-    // printf("decimal view-2: %.25lf", bytesToDouble(uni.ll));
+    // printf("binary view: "); printBinDouble(uni.ll); printf("\n");
+    // // looks pretty fine
+    // for (int i = 0; i < sizeof("binary view: ") - 1; i++) printf(" ");
+    // for (int i = sizeof(double) - 1; i >= 0; i--) printf("^___%u%c__^", i, (i == 7 || i == 6) * '_');
 }
